4575810bb550c197620c99ef154bf60a1da06086
BasToTheMax <bas@arnold-linda.nl>
2022-04-02 16:12:23 +0200

    fix

--- index.js
+++ index.js
@@ -36,10 +36,11 @@ new ssh2.Server({
       });
       session.once('shell', function(accept, reject, info) {
         var stream = accept();
-        done(stream);
 
-        stream.write(fs.readFileSync('./welcomeMessage.txt').toString());
-        stream.write('\r\n'.repeat(3));
+        out += fs.readFileSync('./welcomeMessage.txt').toString();
+        out += '\r\n'.repeat(3);
+
+        done(stream);
 
         stream.on('data', function(data) {
           don == true;
